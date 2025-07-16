cbafaf3447ba4e37470e8d5e34776c894e1bd789
furqank729 <58474453+furqankhalid729@users.noreply.github.com>
2025-07-16 15:07:18 +0500

    Interakt Wenhook

--- app/Http/Controllers/Interakt/Webhook.php
+++ app/Http/Controllers/Interakt/Webhook.php
@@ -14,12 +14,12 @@ class Webhook extends Controller
         $topic = $request->input('type');
         $allowedTopics = ['message_received', 'workflow_response_update'];
 
-        if (!in_array($topic, $allowedTopics)) {
-            Log::warning('Unhandled Interakt Webhook Topic', ['topic' => $topic]);
-            return response()->json(['error' => 'Unhandled topic'], 400);
-        }
-        Log::info('Valid Interakt Webhook Received', $request->all());
+        // if (!in_array($topic, $allowedTopics)) {
+        //     Log::warning('Unhandled Interakt Webhook Topic', ['topic' => $topic]);
+        //     return response()->json(['error' => 'Unhandled topic'], 400);
+        // }
+        // Log::info('Valid Interakt Webhook Received', $request->all());
 
-        return response()->json(['status' => 'success']);
+        return response()->json(['status' => 'success', 'message' => 'Webhook processed successfully'], 200);
     }
 }
