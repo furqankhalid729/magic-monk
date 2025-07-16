0dcca51376d83ff37fbd79cb3e6f87d41bb461fb
furqank729 <58474453+furqankhalid729@users.noreply.github.com>
2025-07-16 15:06:33 +0500

    Interakt Wenhook

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Interakt\Webhook;
 
 Route::post('/webhook', [Webhook::class, 'handle'])
-    ->middleware('verify.interakt')
     ->name('interakt.webhook.handle');
